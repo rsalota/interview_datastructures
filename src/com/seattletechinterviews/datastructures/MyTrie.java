3620e056f28c5e7432cd750f123dd883a13c3cd4
Rahul Salota <rsalota@it-sea14610-mbp.local>
2016-02-14 23:26:22 -0800

    Fixing the interface

--- src/com/seattletechinterviews/datastructures/MyTrie.java
+++ src/com/seattletechinterviews/datastructures/MyTrie.java
@@ -3,12 +3,16 @@ package com.seattletechinterviews.datastructures;
 import java.util.List;
 
 public interface MyTrie {
-	public interface Trie {
-	    public void add(String key, int value);
-	    public int find(String key);
-	    public MyLinkedList search(String prefix);
-	    public boolean contains(String key);
-	    public List<String> getAllKeys();
-	    public int size();
-	}
+
+	public void add(String key, int value);
+
+	public int find(String key);
+
+	public MyLinkedList search(String prefix);
+
+	public boolean contains(String key);
+
+	public List<String> getAllKeys();
+
+	public int size();
 }

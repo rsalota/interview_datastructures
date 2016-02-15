aea5661d0a316367d5d0e3dcfd6682f150cca0bb
Rahul Salota <rsalota@it-sea14610-mbp.local>
2016-02-14 16:46:45 -0800

    Fixed the interface

--- src/com/seattletechinterviews/datastructures/MyHashtable.java
+++ src/com/seattletechinterviews/datastructures/MyHashtable.java
@@ -1,8 +1,8 @@
 package com.seattletechinterviews.datastructures;
 
 public interface MyHashtable {
-	public void putLinkedHashing();
+	public void putLinkedHashing(int key, int value);
 	public int getLinkedHashing();
 	public int getOpenAddressing();
-	public void putOpenAddressing();
+	public void putOpenAddressing(int key, int value);
 }

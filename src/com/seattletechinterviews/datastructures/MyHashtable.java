b887658ee91fa380b25277e3c55a02561684a927
Rahul Salota <rsalota@it-sea14610-mbp.local>
2016-02-14 21:09:32 -0800

    changing the interface

--- src/com/seattletechinterviews/datastructures/MyHashtable.java
+++ src/com/seattletechinterviews/datastructures/MyHashtable.java
@@ -1,8 +1,6 @@
 package com.seattletechinterviews.datastructures;
 
 public interface MyHashtable {
-	public void putLinkedHashing(int key, int value);
-	public int getLinkedHashing();
-	public int getOpenAddressing();
-	public void putOpenAddressing(int key, int value);
+	public void put(int key, int value);
+	public int get(int key);
 }

3ae7af124850e0b0226c1b13f54ccf0706baa62a
Rahul Salota <rsalota@it-sea14610-mbp.local>
2016-02-14 16:52:17 -0800

    Adding heap interface

--- src/com/seattletechinterviews/datastructures/MyHeap.java
+++ src/com/seattletechinterviews/datastructures/MyHeap.java
@@ -1,5 +1,9 @@
 package com.seattletechinterviews.datastructures;
 
 public interface MyHeap {
-
+	public int findMin();
+	public int findMax();
+	public void insert(int value);
+	public void remove(int value);
+	public void merge(MyHeap toMergeHeap);
 }

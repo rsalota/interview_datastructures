91c1519a5ad87e3ff8ac2bbfedc1551a3533fbab
Rahul Salota <rsalota@it-sea14610-mbp.local>
2016-02-14 22:08:54 -0800

    Adding a few more

--- src/com/seattletechinterviews/datastructures/MyTree.java
+++ src/com/seattletechinterviews/datastructures/MyTree.java
@@ -1,4 +1,9 @@
 package com.seattletechinterviews.datastructures;
 
 public interface MyTree {
+	public void printInorder();
+	public void printPreOrder();
+	public void printPostOrder();
+	public void balanceTree();
+	public boolean isBinarySearchTree();
 }
--- src/com/seattletechinterviews/datastructures/MyTrie.java
+++ src/com/seattletechinterviews/datastructures/MyTrie.java
@@ -1,5 +1,14 @@
 package com.seattletechinterviews.datastructures;
 
-public interface MyTrie {
+import java.util.List;
 
+public interface MyTrie {
+	public interface Trie {
+	    public void add(String key, int value);
+	    public int find(String key);
+	    public MyLinkedList search(String prefix);
+	    public boolean contains(String key);
+	    public List<String> getAllKeys();
+	    public int size();
+	}
 }

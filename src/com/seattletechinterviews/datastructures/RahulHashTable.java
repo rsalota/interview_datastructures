57dca5d38bc579c3eb33604c009f809a32520625
Rahul Salota <rsalota@it-sea14610-mbp.local>
2016-02-16 21:45:12 -0800

    adding an implementation

---
+++ src/com/seattletechinterviews/datastructures/RahulHashTable.java
@@ -0,0 +1,17 @@
+package com.seattletechinterviews.datastructures;
+
+public class RahulHashTable implements MyHashtable{
+
+	@Override
+	public void put(int key, int value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int get(int key) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
